4b2958eba3da240f07acbe03fb78906f529a7adc
Tom Everett <tom@khubla.com>
2020-01-01 17:56:54 -0700

    ff

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -jar target/hsinflux-1.0.0-SNAPSHOT.jar --hsurl http://192.168.75.129/JSON --hsuser hs --hspassword hs
--- src/main/java/com/khubla/hsinflux/Importer.java
+++ src/main/java/com/khubla/hsinflux/Importer.java
@@ -1,9 +1,11 @@
 package com.khubla.hsinflux;
 
+import java.io.*;
 import java.util.*;
 
 import com.khubla.hsclient.*;
 import com.khubla.hsclient.domain.*;
+import com.khubla.hsclient.response.*;
 
 public class Importer {
 	public String getHsURL() {
@@ -29,18 +31,42 @@ public class Importer {
 		this.hsUsername = hsUsername;
 	}
 
-	public void run() throws HSClientException {
+	public void run() throws HSClientException, InterruptedException, IOException {
+		Device device = getDevice("Furnace Thermostat", "Z-Wave Temperature");
+		if (device != null) {
+			File f = new File("output.txt");
+			if (false == f.exists()) {
+				f.createNewFile();
+			}
+			FileOutputStream fos = new FileOutputStream("output.txt", true);
+			while (true) {
+				String status = getStatus(device);
+				String s = new Date().getTime() + "," + status + "\n";
+				fos.write(s.getBytes());
+				fos.flush();
+				System.out.print(s);
+				Thread.sleep(1000 * 60);
+			}
+		}
+	}
+
+	public String getStatus(Device device) throws HSClientException {
+		HSClient hsClient = HSClientImpl.connect(hsURL, hsUsername, hsPassword);
+		StatusResponse statusResponse = hsClient.getStatus(device.getRef(), null, null);
+		return (statusResponse.getDevices().get(0).getStatus());
+	}
+
+	public Device getDevice(String parentDeviceName, String subDeviceType) throws HSClientException {
 		HSClient hsClient = HSClientImpl.connect(hsURL, hsUsername, hsPassword);
 		Map<String, Device> deviceMapByName = hsClient.getDevicesByName();
 		Map<Integer, Device> deviceMapByRef = hsClient.getDevicesByRef();
-		for (String name : deviceMapByName.keySet()) {
-			System.out.println(name);
-		}
-		Device mainFloorThermostat = deviceMapByName.get("Furnace Thermostat");
-		System.out.println(Integer.toString(mainFloorThermostat.getRef()));
+		Device mainFloorThermostat = deviceMapByName.get(parentDeviceName);
 		for (Integer ref : mainFloorThermostat.getAssociated_devices()) {
 			Device associatedDevice = deviceMapByRef.get(ref);
-			System.out.println(associatedDevice.getDevice_type_string());
+			if (subDeviceType.compareTo(associatedDevice.getDevice_type_string()) == 0) {
+				return associatedDevice;
+			}
 		}
+		return null;
 	}
 }
