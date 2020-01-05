acbcea43ffce87129bff2a68389bfda6e935641f
Tom Everett <tom@khubla.com>
2020-01-04 23:09:01 -0700

    threaded importer

--- src/main/java/com/khubla/hsinflux/Importer.java
+++ src/main/java/com/khubla/hsinflux/Importer.java
@@ -2,6 +2,7 @@ package com.khubla.hsinflux;
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.*;
 
 import org.influxdb.*;
 import org.influxdb.dto.*;
@@ -11,6 +12,8 @@ import com.khubla.hsclient.domain.*;
 
 public class Importer {
 	private static final String DB_NAME = "house";
+	private static final int THREAD_COUNT = 5;
+	private static final int CYCLE_TIME_MS = 1000 * 60;
 	private final String hsURL;
 	private final String hsPassword;
 	private final String hsUsername;
@@ -74,18 +77,58 @@ public class Importer {
 		if (devices != null) {
 			while (true) {
 				try {
+					final long start = System.currentTimeMillis();
+					/*
+					 * thread pool
+					 */
+					final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+					/*
+					 * list
+					 */
+					final Queue<Point> points = new ConcurrentLinkedQueue<Point>();
 					/*
 					 * walk devices
 					 */
-					final BatchPoints batchPoints = BatchPoints.database(DB_NAME).build();
 					for (final Integer ref : devices.keySet()) {
-						final Device device = updateDevice(ref);
-						final PointGenerator<Device> pointGenerator = new GenericPointGeneratorImpl();
-						final Point point = pointGenerator.generatePoint(device);
-						batchPoints.point(point);
+						/*
+						 * runnable
+						 */
+						final Runnable worker = new Runnable() {
+							@Override
+							public void run() {
+								try {
+									final Device device = updateDevice(ref);
+									final PointGenerator<Device> pointGenerator = new GenericPointGeneratorImpl();
+									final Point point = pointGenerator.generatePoint(device);
+									points.add(point);
+								} catch (final Exception e) {
+									e.printStackTrace();
+								}
+							}
+						};
+						/*
+						 * add to service
+						 */
+						executorService.execute(worker);
 					}
-					writeToInflux(batchPoints);
-					Thread.sleep(1000 * 60);
+					/*
+					 * wait
+					 */
+					executorService.shutdown();
+					executorService.awaitTermination(60, TimeUnit.SECONDS);
+					/*
+					 * write
+					 */
+					writeToInflux(points);
+					/*
+					 * log the time
+					 */
+					final long t = System.currentTimeMillis() - start;
+					System.out.println("Data collection performed in " + Long.toString(t) + " ms on " + THREAD_COUNT + " threads");
+					/*
+					 * nap time
+					 */
+					Thread.sleep(CYCLE_TIME_MS - t);
 				} catch (final Exception e) {
 					e.printStackTrace();
 				}
@@ -103,15 +146,29 @@ public class Importer {
 		}
 	}
 
-	private void writeToInflux(BatchPoints batchPoints) {
-		InfluxDB influxDB = null;
-		try {
-			influxDB = InfluxDBFactory.connect(influxURL, influxUsername, influxPassword);
-			influxDB.write(batchPoints);
-		} catch (final Exception e) {
-			e.printStackTrace();
-		} finally {
-			influxDB.close();
+	/**
+	 * write to influx on a thread
+	 *
+	 * @param batchPoints
+	 */
+	private void writeToInflux(Queue<Point> points) {
+		/*
+		 * make the batch
+		 */
+		final BatchPoints batchPoints = BatchPoints.database(DB_NAME).build();
+		for (final Point point : points) {
+			batchPoints.point(point);
 		}
+		new Thread(() -> {
+			InfluxDB influxDB = null;
+			try {
+				influxDB = InfluxDBFactory.connect(influxURL, influxUsername, influxPassword);
+				influxDB.write(batchPoints);
+			} catch (final Exception e) {
+				e.printStackTrace();
+			} finally {
+				influxDB.close();
+			}
+		}).start();
 	}
 }
