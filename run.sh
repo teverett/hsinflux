bc55ce2f2a2d64a7cc971dfb7376bda1fe124151
Tom Everett <tom@khubla.com>
2020-01-11 17:14:27 -0700

    updated run.sh to expose jmx

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-java -jar target/hsinflux-1.0.0-SNAPSHOT.jar
+java -jar \
+-Djava.rmi.server.hostname=192.168.75.71 \
+-Dcom.sun.management.jmxremote \
+-Dcom.sun.management.jmxremote.port=9010 \
+-Dcom.sun.management.jmxremote.local.only=false \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
+target/hsinflux-1.0.0-SNAPSHOT.jar
