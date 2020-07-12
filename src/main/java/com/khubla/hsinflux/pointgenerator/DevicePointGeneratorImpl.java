18af448c9a180d2f51db8c6a843a901299c195c4
Tom Everett <tom@khubla.com>
2020-07-11 19:04:39 -0600

    name

--- src/main/java/com/khubla/hsinflux/pointgenerator/DevicePointGeneratorImpl.java
+++ src/main/java/com/khubla/hsinflux/pointgenerator/DevicePointGeneratorImpl.java
@@ -5,6 +5,7 @@ import java.util.concurrent.*;
 import org.influxdb.dto.*;
 
 import com.khubla.hsclient.poll.*;
+import com.khubla.hsclient.util.*;
 import com.khubla.hsinflux.*;
 
 /**
@@ -20,22 +21,12 @@ public class DevicePointGeneratorImpl implements PointGenerator<DataPoint> {
 	public Point generatePoint(DataPoint dataPoint) {
 		try {
 			final Point point = Point.measurement(MEASUREMENT_NAME).tag("ref", dataPoint.getDeviceRef().toString()).tag("location1", dataPoint.getLocation()).tag("location2", dataPoint.getLocation2())
-					.tag("name", getDeviceName(dataPoint.getName())).tag("type", dataPoint.getType()).time(System.currentTimeMillis(), TimeUnit.MILLISECONDS).addField("value", dataPoint.getValue())
-					.addField("status", dataPoint.getStatus()).addField("lastchange", dataPoint.getLastChange()).build();
+					.tag("name", DeviceName.getDeviceName(dataPoint.getName())).tag("type", dataPoint.getType()).time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+					.addField("value", dataPoint.getValue()).addField("status", dataPoint.getStatus()).addField("lastchange", dataPoint.getLastChange()).build();
 			return point;
 		} catch (final Exception e) {
 			e.printStackTrace();
 			return null;
 		}
-	}
-
-	private String getDeviceName(String n) {
-		final StringBuilder stringBuilder = new StringBuilder();
-		for (int i = 0; i < n.length(); i++) {
-			if ((((n.charAt(i) >= 'a') && (n.charAt(i) <= 'z'))) || (((n.charAt(i) >= 'A') && (n.charAt(i) <= 'Z')))) {
-				stringBuilder.append(n.charAt(i));
-			}
-		}
-		return stringBuilder.toString();
 	}
 }
